330cf3b471d28b196674cba5dfebe0530a25488a
Adolfo Chafardett <adolfo1997a@gmail.com>
2024-10-27 16:14:48 -0400

    feature:[allow * origins]

--- ds_api/src/main.js
+++ ds_api/src/main.js
@@ -7,12 +7,13 @@ const modules = require('./modules')
 const port = process.env.APP_PORT
 const app = express()
 
-app.use(cors())
+app.use(cors({
+  origin: ['*']
+}))
 app.use(express.json())
 app.use('/api/v1', modules)
 
 app.listen(port, async () => {
-  
   await mongoose.connect(`${process.env.APP_MONGODB_URI}`, {
     useNewUrlParser: true,
   });
